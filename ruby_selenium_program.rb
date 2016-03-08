2677f43229c3ac91945d6ca2a0bd1e8fb1cdda76
lilybarrett <lily@barrett.us>
2016-03-08 15:44:32 -0500

    added new testing commands

--- ruby_selenium_program.rb
+++ ruby_selenium_program.rb
@@ -5,13 +5,20 @@ driver = Selenium::WebDriver.for :firefox
 driver.get "http://google.com"
 
 element = driver.find_element :name => "q"
-element.send_keys "Cheese!"
+element.send_keys "star wars"
 element.submit
 
 puts "Page title is #{driver.title}"
 
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-wait.until { driver.title.downcase.start_with? "cheese!" }
+
+puts "Test Passed 1" if wait.until {
+  driver.find_element(:id, "logocont").displayed?
+}
+
+puts "Test Passed 2" if wait.until {
+  driver.find_element(:css, "div.xpdopen").displayed?
+}
 
 puts "Page title is #{driver.title}"
 driver.quit
